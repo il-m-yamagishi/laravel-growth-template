817eab92ce30502d932e65079142de2003d051de
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2022-12-25 22:05:47 +0900

    feat: laravel -> php-packages/api-server

--- php-packages/api-server/app/Providers/RouteServiceProvider.php
+++ php-packages/api-server/app/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/api';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -33,10 +33,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->group(base_path('route.php'));
         });
     }
 
