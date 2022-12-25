d9af1d70eb6cb91d0890f3211d09c39fbd190012
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2022-12-25 18:42:28 +0900

    chore: add psalm

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->dateTime('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->dateTime('created_at');
